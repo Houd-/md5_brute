631aaedb8248aea6fed3c6ed38b07bf84190b2f7
Houd <houd@houdhacks.info>
2014-01-13 17:42:21 -0600

    Added md5 function.

--- md5.c
+++ md5.c
@@ -16,24 +16,31 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <openssl/md5.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* hash;
 char* wordlist;
 
 int process_args(int argc, char* argv[]);
+int load_list();
+char *str2md5(const char *str, int length);
 
 int main(int argc, char* argv[]) {
 	if (argc == 1) {
 		printf("MD5 Brute Force - Usage:\n");
-		printf("./md5_brute -h <hash> -w <wordlist>");
+		printf("./md5_brute -h <hash> -w <wordlist>\n");
 		goto exit;
 	}
 	if (process_args(argc, argv) == -1)
 		goto halt;
+	if (load_list() == -1)
+		goto halt;
+
 	return 0;
-	halt: printf("An error occured.");
+	halt: printf("An error occured.\n");
 	return -1;
 	exit: return -1;
 }
@@ -46,12 +53,44 @@ int process_args(int argc, char* argv[]) {
 		if (strcmp(argv[i], "-h")) {
 			i++;
 			hash = argv[i];
-			printf("Hash: %s\n",hash);
+			printf("Hash: %s\n", hash);
 		} else if (strcmp(argv[i], "-w")) {
 			i++;
 			wordlist = argv[i];
-			printf("Using list: %s\n",wordlist);
+			printf("Using list: %s\n", wordlist);
 		}
 	}
 	return 0;
+}
+
+int load_list() {
+
+	return 0;
+}
+
+char *str2md5(const char *str, int length) {
+	int n;
+	MD5_CTX c;
+	unsigned char digest[16];
+	char *out = (char*) malloc(33);
+
+	MD5_Init(&c);
+
+	while (length > 0) {
+		if (length > 512) {
+			MD5_Update(&c, str, 512);
+		} else {
+			MD5_Update(&c, str, length);
+		}
+		length -= 512;
+		str += 512;
+	}
+
+	MD5_Final(digest, &c);
+
+	for (n = 0; n < 16; ++n) {
+		snprintf(&(out[n * 2]), 16 * 2, "%02x", (unsigned int) digest[n]);
+	}
+
+	return out;
 }
